59c9934b2effe7d75fd647a9609fec81b7e9cbef
James <james.bidule@gmail.com>
2025-04-11 15:30:29 +0200

    chore: edit text to craigslist

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="bg-gray-100 py-4 text-center">
-      <p class="text-gray-500">&copy; {{ currentYear }} Spark Vehicles Explorer | Made with ❤️ by JMV BCR Coders</p>
+      <p class="text-gray-500">&copy; {{ currentYear }} Craigslist Vehicles Explorer | Made with ❤️ by JMV BCR Coders</p>
     </footer>
   </template>
   
